a26da81e7e4b2343cff3395565fd4ca2071e6620
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2023-02-01 17:16:37 +0400

    fixed moveto pythonautogui

--- data/hearthstone_bot.py
+++ data/hearthstone_bot.py
@@ -158,7 +158,7 @@ class HearthstoneBot:
         """
 
         pyautogui.click(self.target_x, self.target_y)
-        pyautogui.moveTo(0, 0)
+        pyautogui.moveTo(10, 10)
 
 
     def search_and_click_on_target(
